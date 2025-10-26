8bb71e4f75506e81088cbf1d13c747c313d56f9a
MuhammedZeki <muhammed.codes@gmail.com>
2025-10-26 15:46:14 +0300

    AllNotes responsive is done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,18 +6,22 @@ import { FaHashtag } from "react-icons/fa";
 import { GoClock } from "react-icons/go";
 import { IoArchiveOutline } from "react-icons/io5";
 import { RiDeleteBin5Line } from "react-icons/ri";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { TiHomeOutline } from "react-icons/ti";
+import { GoTag } from "react-icons/go";
+
 const Home = () => {
   return (
     <div className="h-screen flex">
-      <div className="w-[20%]">
+      <div className="lg:w-[20%] lg:block hidden">
         <SideBar />
       </div>
-      <div className="w-[80%] bg-[#0E121B] flex flex-col">
+      <div className="w-full lg:w-[80%]  bg-[#0E121B] flex flex-col">
         <div className="flex items-center justify-between px-8 py-4 border-b border-b-[#232530]">
           <span className="text-[#E0E4EA] font-inter font-bold text-2xl tracking-[-0.5px] leading-[120%]">
             All Notes
           </span>
-          <div className="flex items-center gap-6">
+          <div className="lg:flex lg:items-center lg:gap-6 hidden">
             <label
               className="border rounded-lg border-[#2B303B] flex items-center px-5 py-3 gap-2"
               htmlFor="search"
@@ -35,7 +39,7 @@ const Home = () => {
           </div>
         </div>
         <div className="h-screen flex">
-          <div className="bg-[#0E121B] w-[25%] border-r border-r-[#232530] flex flex-col gap-4 px-7 py-5">
+          <div className="bg-[#0E121B] lg:w-[25%]  hidden border-r border-r-[#232530] lg:flex lg:flex-col lg:gap-4 lg:px-7 lg:py-5">
             <button className="flex items-center justify-center gap-2 bg-[#335CFF] rounded-lg px-4 py-3 text-[#E0E4EA] font-,nter font-medium tracking-[120%] leading-[-0.2px] text-md">
               <GoPlus className="text-[#E0E4EA] w-6 h-6" />
               Create New Note
@@ -136,7 +140,25 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="bg-[#0E121B] w-[55%] border-r border-r-[#232530] px-6 py-5 flex flex-col gap-4">
+          <div className="bg-[#0E121B] w-full lg:w-[55%] border-r border-r-[#232530] px-6 py-5 flex flex-col gap-4">
+            <div className="flex items-center justify-between w-full lg:hidden">
+              <div className="flex items-center gap-1">
+                <MdKeyboardArrowLeft className="text-[#CACFD8] w-6 h-6" />
+                <span className="text-[#CACFD8] font-inter font-normal text-sm tracking-[130%] leading-[-0.2px]">
+                  Back
+                </span>
+              </div>
+              <div className="flex items-center justify-center gap-4">
+                <RiDeleteBin5Line className="text-[#CACFD8] w-5 h-5" />
+                <IoArchiveOutline className="text-[#CACFD8] w-5 h-5" />
+                <span className="text-[#CACFD8] font-inter font-normal text-sm tracking-[130%] leading-[-0.2px]">
+                  Cancel
+                </span>
+                <span className="text-[#335CFF] font-inter font-normal text-sm tracking-[130%] leading-[-0.2px]">
+                  Save Note
+                </span>
+              </div>
+            </div>
             <div className="text-[#E0E4EA] font-inter font-bold text-2xl leading-[-0.5px] tracking-[120%]">
               React Performance Optimization
             </div>
@@ -164,9 +186,8 @@ const Home = () => {
               <textarea
                 name=""
                 id=""
-                cols={80}
                 rows={22}
-                className=" text-[#CACFD8] font-inter font-normal text-sm tracking-[130%] leading-[-0.2px] py-2 resize-none"
+                className="w-full text-[#CACFD8] font-inter font-normal text-sm tracking-[130%] leading-[-0.2px] py-2 resize-none"
               >
                 Key performance optimization techniques: 1. Code Splitting - Use
                 React.lazy() for route-based splitting - Implement dynamic
@@ -178,7 +199,7 @@ const Home = () => {
                 identify bottlenecks
               </textarea>
             </div>
-            <div className="flex items-center gap-4 pt-4">
+            <div className="hidden lg:flex lg:items-center lg:gap-4 lg:pt-4">
               <button className="bg-[#335CFF] text-white rounded-lg px-4 py-3 font-inter font-medium text-sm tracking-[120%] leading-[-0.2px]">
                 Save Note
               </button>
@@ -186,8 +207,47 @@ const Home = () => {
                 Cancel
               </button>
             </div>
+            <div className="lg:hidden mt-6 flex items-center justify-around">
+              <div className="flex flex-col items-center justify-center gap-2 px-5 py-2.5 sm:py-1 bg-[#2B303B] rounded-sm">
+                <TiHomeOutline className="text-[#335CFF] w-5 h-5" />
+                <span className="text-[#335CFF] hidden sm:block font-inter font-normal text-xs tracking-[120%] leading-[-0.2px]">
+                  Home
+                </span>
+              </div>
+              <div className=" h-12 border-r border-r-[#232530] hidden sm:block"></div>
+              <div className="flex flex-col items-center justify-center gap-2 px-5 py-1  rounded-sm">
+                <IoIosSearch className="text-[#99A0AE] w-5 h-5" />
+                <span className="text-[#99A0AE] hidden sm:block font-inter font-normal text-xs tracking-[120%] leading-[-0.2px]">
+                  Search
+                </span>
+              </div>
+              <div className=" h-12 border-r border-r-[#232530] hidden sm:block"></div>
+
+              <div className="flex flex-col items-center justify-center gap-2 px-5 py-1  rounded-sm">
+                <IoArchiveOutline className="text-[#99A0AE] w-5 h-5" />
+                <span className="text-[#99A0AE] hidden sm:block font-inter font-normal text-xs tracking-[120%] leading-[-0.2px]">
+                  Archived
+                </span>
+              </div>
+              <div className=" h-12 border-r border-r-[#232530] hidden sm:block"></div>
+
+              <div className="flex flex-col items-center justify-center gap-2 px-5 py-1  rounded-sm">
+                <GoTag className="text-[#99A0AE] w-5 h-5" />
+                <span className="text-[#99A0AE] hidden sm:block font-inter font-normal text-xs tracking-[120%] leading-[-0.2px]">
+                  Tags
+                </span>
+              </div>
+              <div className=" h-12 border-r border-r-[#232530] hidden sm:block"></div>
+
+              <div className="flex flex-col items-center justify-center gap-2 px-5 py-1  rounded-sm">
+                <CiSettings className="text-[#99A0AE] w-5 h-5" />
+                <span className="text-[#99A0AE] hidden sm:block font-inter font-normal text-xs tracking-[120%] leading-[-0.2px]">
+                  Settings
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="bg-[#0E121B] w-[20%] px-6 py-5 flex flex-col gap-4">
+          <div className="bg-[#0E121B] hidden  lg:w-[20%] lg:px-6 lg:py-5 lg:flex lg:flex-col lg:gap-4">
             <div className="border border-[#232530] rounded-lg flex items-center gap-4 px-4 py-3">
               <IoArchiveOutline className="text-[#CACFD8] w-5 h-5" />
               <span className="font-inter font-medium tracking-[120%] leading-[-0.2px] text-sm text-[#CACFD8]">
